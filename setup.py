3c35ea776ef8a3ef5634a9a31b432ac1bdd8a0e8
Ryan Galloway <ryan@rsg.io>
2024-08-14 10:18:40 -0700

    formatting updates on setup file

--- setup.py
+++ setup.py
@@ -9,19 +9,20 @@ from distutils.core import setup
 from pyseq import __version__
 
 from os import path
+
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md')) as f:
+with open(path.join(this_directory, "README.md")) as f:
     long_description = f.read()
 
 setup(
-    name='pyseq',
+    name="pyseq",
     version=__version__,
-    description='Compressed Sequence String Module',
+    description="Compressed File Sequence String Module",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Ryan Galloway',
-    author_email='ryan@rsgalloway.com',
-    url='http://github.com/rsgalloway/pyseq',
-    py_modules=['pyseq'],
-    scripts = ['lss']
+    long_description_content_type="text/markdown",
+    author="Ryan Galloway",
+    author_email="ryan@rsgalloway.com",
+    url="http://github.com/rsgalloway/pyseq",
+    py_modules=["pyseq"],
+    scripts=["lss"],
 )
